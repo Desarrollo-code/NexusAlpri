32e96cf86f7de673f1ef256afd2c009a60d5a8ea
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-07-18 20:36:34 +0000

    Revierte estos cambios que acabas de hacer:( cd11a81

--- src/components/ui/alert-dialog.tsx
+++ src/components/ui/alert-dialog.tsx
@@ -38,7 +38,6 @@ const AlertDialogContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
-        "dark:glass-card",
         className
       )}
       {...props}
@@ -67,7 +66,7 @@ const AlertDialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end gap-2",
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
       className
     )}
     {...props}
@@ -93,7 +92,7 @@ const AlertDialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-muted-foreground", {[className || '']: true, 'text-balance': true})}
+    className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
 ))
@@ -120,7 +119,7 @@ const AlertDialogCancel = React.forwardRef<
     ref={ref}
     className={cn(
       buttonVariants({ variant: "outline" }),
-      "mt-0",
+      "mt-2 sm:mt-0",
       className
     )}
     {...props}
