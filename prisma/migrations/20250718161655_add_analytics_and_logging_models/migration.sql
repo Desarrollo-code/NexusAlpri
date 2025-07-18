54898ce1694c0338b071fad0d0d110da0aa17a8c
vane24ofc <vaneaueisksow58@gmail.com>
2025-07-18 11:17:08 -0500

    .

---
+++ prisma/migrations/20250718161655_add_analytics_and_logging_models/migration.sql
@@ -0,0 +1,280 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'STUDENT',
+    `registeredDate` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `isTwoFactorEnabled` BOOLEAN NOT NULL DEFAULT false,
+    `twoFactorSecret` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Course` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `imageUrl` VARCHAR(191) NULL,
+    `category` VARCHAR(191) NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'DRAFT',
+    `publicationDate` DATETIME(3) NULL,
+    `instructorId` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Course_instructorId_idx`(`instructorId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Module` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+    `courseId` VARCHAR(191) NOT NULL,
+
+    INDEX `Module_courseId_idx`(`courseId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Lesson` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+    `moduleId` VARCHAR(191) NOT NULL,
+
+    INDEX `Lesson_moduleId_idx`(`moduleId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ContentBlock` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `content` TEXT NULL,
+    `order` INTEGER NOT NULL,
+    `lessonId` VARCHAR(191) NOT NULL,
+
+    INDEX `ContentBlock_lessonId_idx`(`lessonId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Quiz` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `contentBlockId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Quiz_contentBlockId_key`(`contentBlockId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Question` (
+    `id` VARCHAR(191) NOT NULL,
+    `text` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL DEFAULT 'MULTIPLE_CHOICE',
+    `order` INTEGER NOT NULL,
+    `quizId` VARCHAR(191) NOT NULL,
+
+    INDEX `Question_quizId_idx`(`quizId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AnswerOption` (
+    `id` VARCHAR(191) NOT NULL,
+    `text` VARCHAR(191) NOT NULL,
+    `isCorrect` BOOLEAN NOT NULL DEFAULT false,
+    `feedback` TEXT NULL,
+    `questionId` VARCHAR(191) NOT NULL,
+
+    INDEX `AnswerOption_questionId_idx`(`questionId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Enrollment` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `courseId` VARCHAR(191) NOT NULL,
+    `enrolledAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `Enrollment_userId_idx`(`userId`),
+    INDEX `Enrollment_courseId_idx`(`courseId`),
+    UNIQUE INDEX `Enrollment_userId_courseId_key`(`userId`, `courseId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CourseProgress` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `courseId` VARCHAR(191) NOT NULL,
+    `progressPercentage` DOUBLE NOT NULL DEFAULT 0,
+    `enrollmentId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `CourseProgress_enrollmentId_key`(`enrollmentId`),
+    INDEX `CourseProgress_userId_idx`(`userId`),
+    INDEX `CourseProgress_courseId_idx`(`courseId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LessonCompletionRecord` (
+    `id` VARCHAR(191) NOT NULL,
+    `progressId` VARCHAR(191) NOT NULL,
+    `lessonId` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `score` DOUBLE NULL,
+
+    INDEX `LessonCompletionRecord_progressId_idx`(`progressId`),
+    UNIQUE INDEX `LessonCompletionRecord_progressId_lessonId_key`(`progressId`, `lessonId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Resource` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NULL,
+    `category` VARCHAR(191) NOT NULL,
+    `tags` VARCHAR(191) NULL,
+    `pin` VARCHAR(191) NULL,
+    `uploadDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `uploaderId` VARCHAR(191) NULL,
+    `parentId` VARCHAR(191) NULL,
+
+    INDEX `Resource_parentId_idx`(`parentId`),
+    INDEX `Resource_uploaderId_idx`(`uploaderId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Announcement` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
+    `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `priority` VARCHAR(191) NOT NULL DEFAULT 'Normal',
+    `authorId` VARCHAR(191) NULL,
+    `audience` JSON NOT NULL,
+
+    INDEX `Announcement_authorId_idx`(`authorId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Notification` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `link` VARCHAR(191) NULL,
+    `read` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `Notification_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CalendarEvent` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `location` VARCHAR(191) NULL,
+    `start` DATETIME(3) NOT NULL,
+    `end` DATETIME(3) NOT NULL,
+    `allDay` BOOLEAN NOT NULL DEFAULT false,
+    `color` VARCHAR(191) NOT NULL DEFAULT 'blue',
+    `videoConferenceLink` VARCHAR(191) NULL,
+    `audienceType` VARCHAR(191) NOT NULL DEFAULT 'ALL',
+    `attachments` JSON NOT NULL,
+    `creatorId` VARCHAR(191) NOT NULL,
+
+    INDEX `CalendarEvent_creatorId_idx`(`creatorId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `LessonTemplate` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `creatorId` VARCHAR(191) NULL,
+    `baseLessonId` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `LessonTemplate_baseLessonId_key`(`baseLessonId`),
+    INDEX `LessonTemplate_creatorId_idx`(`creatorId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TemplateBlock` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+    `templateId` VARCHAR(191) NOT NULL,
+
+    INDEX `TemplateBlock_templateId_idx`(`templateId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `PlatformSettings` (
+    `id` VARCHAR(191) NOT NULL,
+    `platformName` VARCHAR(191) NOT NULL DEFAULT 'NexusAlpri',
+    `allowPublicRegistration` BOOLEAN NOT NULL DEFAULT true,
+    `enableEmailNotifications` BOOLEAN NOT NULL DEFAULT true,
+    `emailWhitelist` VARCHAR(191) NULL,
+    `passwordMinLength` INTEGER NOT NULL DEFAULT 8,
+    `passwordRequireUppercase` BOOLEAN NOT NULL DEFAULT true,
+    `passwordRequireLowercase` BOOLEAN NOT NULL DEFAULT true,
+    `passwordRequireNumber` BOOLEAN NOT NULL DEFAULT true,
+    `passwordRequireSpecialChar` BOOLEAN NOT NULL DEFAULT true,
+    `resourceCategories` TEXT NULL DEFAULT 'Recursos Humanos,TI y Seguridad,Marketing,Ventas,Legal,Operaciones,Finanzas,Formación Interna,Documentación de Producto,General',
+    `enableIdleTimeout` BOOLEAN NOT NULL DEFAULT true,
+    `idleTimeoutMinutes` INTEGER NOT NULL DEFAULT 20,
+    `require2faForAdmins` BOOLEAN NOT NULL DEFAULT false,
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `SecurityLog` (
+    `id` VARCHAR(191) NOT NULL,
+    `event` VARCHAR(191) NOT NULL,
+    `ipAddress` VARCHAR(191) NULL,
+    `details` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NULL,
+    `emailAttempt` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `SecurityLog_userId_idx`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_EventAttendees` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_EventAttendees_AB_unique`(`A`, `B`),
+    INDEX `_EventAttendees_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
