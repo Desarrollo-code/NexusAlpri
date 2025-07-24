5692d3d3f5ebd7702cea9ed63e1618f1da6541a8
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-07-24 15:18:10 +0000

    Quiero los bordes redondeados...

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -79,9 +79,9 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 4px)",
+        sm: "calc(var(--radius) - 8px)",
       },
       keyframes: {
         'accordion-down': {
