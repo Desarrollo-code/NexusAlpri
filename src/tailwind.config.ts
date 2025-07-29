0b0c02073fb5256b5d49f1fe920abb838c414e54
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-07-29 20:18:18 +0000

    Quiero que sea más dinamico, que se vea bonito

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -114,11 +114,15 @@ export default {
             '0%': { transform: 'translateY(20px) scale(0.95)', opacity: '0' },
             '100%': { transform: 'translateY(0) scale(1)', opacity: '1' }
         },
-        ping: {
+        'ping': {
           '75%, 100%': {
             transform: 'scale(2)',
             opacity: '0',
           },
+        },
+        'caret-blink': {
+          '0%, 70%, 100%': { opacity: '1' },
+          '20%, 50%': { opacity: '0' },
         },
       },
       animation: {
@@ -126,7 +130,8 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'press': 'press 0.2s 1 linear',
         'throw': 'throw 0.4s ease-out forwards',
-        ping: 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'ping': 'ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'caret-blink': 'caret-blink 1.25s ease-out infinite',
       },
     },
   },
