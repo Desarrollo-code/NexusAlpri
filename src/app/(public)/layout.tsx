7c02e5a0b1ca16795f9e392375b8852d1643709e
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-08-04 14:36:11 +0000

    GET http://localhost:9002/ 500 (Internal Server Error) [NEW] Explain Con

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -10,7 +10,11 @@ export default function PublicLayout({
   return (
     <div className="flex flex-col w-full flex-1">
       <PublicTopBar />
-      <main className="flex-1 flex flex-col">{children}</main>
+      <main className="flex-1 flex flex-col items-center justify-center py-12 md:py-24">
+        <div className="w-full max-w-7xl mx-auto p-4 md:p-0">
+          {children}
+        </div>
+      </main>
     </div>
   );
 }
