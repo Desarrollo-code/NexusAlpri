128fc03410694268a1ed30ef24adf0ed60912515
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-08-05 12:57:18 +0000

    Quita la barra de busqueda en la barra de navegación lateral

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -63,13 +63,6 @@ function AppLayout({ children }: { children: React.ReactNode }) {
                       </div>
                 </SidebarHeader>
 
-                 <div className="p-4">
-                    <div className="relative">
-                      <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
-                      <Input type="text" id="sidebar-search" name="sidebar-search" placeholder="Buscar..." className="w-full pl-10 pr-4 py-2 bg-gray-800 border border-gray-600 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:border-blue-500 h-9 sidebar-text" />
-                    </div>
-                  </div>
-                
                 <SidebarContent />
                 
                 <SidebarFooter>
