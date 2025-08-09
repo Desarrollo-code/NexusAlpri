546fb19750e41ea64c7f11669532940a41afe666
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-08-09 16:33:48 +0000

    Quiero que también cuando me encuentre en la sección acceder aparezca...

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/(auth)/layout.tsx
 import { Footer } from '@/components/layout/footer';
 import { PublicTopBar } from '@/components/layout/public-top-bar';
+import { BottomNav } from '@/components/layout/bottom-nav';
 
 export default function AuthLayout({
   children,
@@ -10,10 +11,15 @@ export default function AuthLayout({
   return (
     <div className="flex flex-col min-h-screen bg-background">
        <PublicTopBar />
-       <main className="flex-1 flex flex-col items-center justify-center p-4">
+       <main className="flex-1 flex flex-col items-center justify-center p-4 pt-24 md:pt-4">
         {children}
        </main>
-       <Footer />
+       <div className="hidden md:block z-10">
+        <Footer />
+      </div>
+       <div className="z-10">
+        <BottomNav />
+      </div>
     </div>
   );
 }
