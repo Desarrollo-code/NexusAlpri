315f57bb2b3389f16e8c672405d1e25830fdc675
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-08-12 16:24:28 +0000

    React.Children.only expected to receive a single React element child.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold ring-offset-background transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative overflow-hidden group",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold ring-offset-background transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative overflow-hidden group gap-2",
   {
     variants: {
       variant: {
@@ -45,9 +45,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     
-    // Check if children is an array and if the first element is a Lucide icon
-    const hasIconOnLeft = React.Children.count(children) > 1 && React.isValidElement(React.Children.toArray(children)[0]);
-
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
@@ -59,8 +56,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           variant === 'primary-gradient' ? 'opacity-100 group-hover:opacity-100' : 'opacity-0 group-hover:opacity-10'
         )} />
         <span className={cn(
-            "inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-card/95 dark:bg-background/95 px-3 py-1 text-sm font-medium text-foreground backdrop-blur-3xl",
-            hasIconOnLeft && "gap-2" // Añade el espaciado aquí
+            "inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-card/95 dark:bg-background/95 px-3 py-1 text-sm font-medium text-foreground backdrop-blur-3xl"
         )}>
           {children}
         </span>
