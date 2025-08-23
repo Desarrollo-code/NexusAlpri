3de348976d67d58266b4003b458a315415417db9
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-08-23 14:17:30 +0000

    ## Error Type Build Error
    ## Error Message
    You cannot have two parallel

--- src/app/forms/[formId]/view/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/forms/[formId]/view/page.tsx
-import { FormViewer } from '@/components/forms/form-viewer';
-import React from 'react';
-
-export default function ViewFormPage({ params }: { params: { formId: string } }) {
-  const { formId } = params;
-
-  return <FormViewer formId={formId} />;
-}
