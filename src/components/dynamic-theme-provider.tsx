920844aeea41b80914343ccee523595c40e95d27
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-08-25 15:00:54 +0000

    Oyep, recuerda que el tema que seleccione el usuario en el mundo privado

--- src/components/dynamic-theme-provider.tsx
+++
@@ -1,3 +0,0 @@
-// This file is no longer needed and can be deleted.
-// The logic has been moved into src/components/theme-provider.tsx
-// to simplify the theme management and fix the dark/light mode transition issue.
