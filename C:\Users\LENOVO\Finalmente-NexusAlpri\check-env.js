39c7e288918fb463f03c95a43cf6d31c87cd153e
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-09-01 19:18:40 +0000

    Pero como envío estos cambio, los que estas realizando tu alla?

---
+++ C:\Users\LENOVO\Finalmente-NexusAlpri\check-env.js
@@ -0,0 +1,6 @@
+// Este script ayuda a depurar las variables de entorno durante el despliegue en Render.
+console.log("--- Checking Environment Variables ---");
+console.log("DATABASE_URL is set:", !!process.env.DATABASE_URL);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("JWT_SECRET is set:", !!process.env.JWT_SECRET);
+console.log("------------------------------------");
