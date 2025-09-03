adf684cad40d9e691c87ea2be793271c89dd3df1
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-09-03 22:15:44 +0000

    Sigue igual...

--- next.config.js
+++ next.config.js
@@ -8,28 +8,16 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true, // Sirve las imágenes estáticamente sin optimización
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.ytimg.com',
-        port: '',
-        pathname: '/**',
       },
-       // Añadimos el dominio de la aplicación para permitir imágenes locales
-      {
-        protocol: 'https',
-        hostname: process.env.RENDER_EXTERNAL_HOSTNAME,
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      }
     ],
   },
 };
