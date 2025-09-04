b5372b709df4e0b4caf6a767363894a448c43e24
alprigrama Desarrollo <desarrolloalprigrama@gmail.com>
2025-09-04 14:12:07 +0000

    Revisa que así deba estar en las tres paginas publicas, solo con una bar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@
 import { PublicTopBar } from '@/components/layout/public-top-bar';
 import React from 'react';
 import { DecorativeHeaderBackground } from '@/components/layout/decorative-header-background';
+import { BottomNav } from '@/components/layout/bottom-nav';
 
 export default function AuthLayout({
   children,
@@ -12,9 +13,10 @@ export default function AuthLayout({
     <div className="relative flex flex-col min-h-screen isolate">
         <DecorativeHeaderBackground />
         <PublicTopBar />
-        <main className="flex-1 flex flex-col items-center justify-center p-4">
+        <main className="flex-1 flex flex-col items-center justify-center p-4 pb-16 md:pb-4">
             {children}
         </main>
+        <BottomNav />
     </div>
   );
 }
