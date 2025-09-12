a6bac93c48cbba8d43e3f237be4938a21902a09a
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-09-12 16:11:57 +0000

    .

--- src/components/layout/app-watermark.tsx
+++ src/components/layout/app-watermark.tsx
@@ -16,14 +16,14 @@ export default function AppWatermark() {
       return (
         <div className={cn(
             "fixed z-[9999] pointer-events-none",
-            "bottom-16 ml-75", // Posición en móvil
+            "bottom-16 ml-80", // Posición en móvil
             "md:bottom-4 md:left-auto md:right-4 md:transform-none md:ml-0" // Posición en escritorio
         )}>
           <Image 
             src={settings.watermarkUrl} 
             alt="Marca de agua" 
-            width={80} 
-            height={80} 
+            width={75} 
+            height={75} 
             className="opacity-50" 
             data-ai-hint="logo company" 
             priority 
