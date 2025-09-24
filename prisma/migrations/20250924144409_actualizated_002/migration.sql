2a7c6d35b84668a6ed0b20882e23b859ed4ea0ae
vane24ofc <vaneaueisksow58@gmail.com>
2025-09-24 12:40:43 -0500

    .

--- prisma/migrations/20250924144409_actualizated_002/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `audience` column on the `Announcement` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Announcement" DROP COLUMN "audience",
-ADD COLUMN     "audience" "EventAudienceType" NOT NULL DEFAULT 'ALL';
