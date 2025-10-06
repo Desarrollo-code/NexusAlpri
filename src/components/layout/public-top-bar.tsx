a545d57ddfa4bd8fef5732a7dbb73f6ef12ec16c
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-10-06 19:17:01 +0000

    Amplia el logo

--- src/components/layout/public-top-bar.tsx
+++ src/components/layout/public-top-bar.tsx
@@ -28,7 +28,7 @@ export function PublicTopBar() {
       <div className="container mx-auto flex items-center justify-between px-4 lg:px-6 h-16">
         <div className="flex items-center justify-start flex-1">
           <Link href="/" className="flex items-center justify-center gap-3" prefetch={false}>
-            <div className={cn("w-10 h-10 flex items-center justify-center flex-shrink-0 rounded-lg relative overflow-hidden")}>
+            <div className={cn("w-12 h-12 flex items-center justify-center flex-shrink-0 rounded-lg relative overflow-hidden")}>
               {settings?.logoUrl ? (
                   <div className="relative w-full h-full">
                       <Image src={settings.logoUrl} alt="Logo" fill data-ai-hint="logo" quality={100} className="object-contain" />
