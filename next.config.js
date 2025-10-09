e18ad0f8b1885b2a9af16ebdb972a51abf72e7f0
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-10-09 15:51:32 +0000

    Es que quiero llevarlo a otro nivel, poder etidar todo tipo de archivo,

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    // This is to make `react-pdf` work with Next.js
+    config.resolve.alias.canvas = false;
+    return config;
+  },
   images: {
     remotePatterns: [
       {
