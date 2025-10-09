277f2247f3d41c7cd458102dae127f21d8527457
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-10-09 17:14:45 +0000

    Recuerda que4 el proyecto ya esta en producción, no entiendo el porque m

---
+++ vercel/path0/next.config.js
@@ -0,0 +1,55 @@
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    // This is to make `react-pdf` work with Next.js
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'izefimwyuayfvektsstg.supabase.co',
+        pathname: '/storage/v1/object/public/avatars/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'izefimwyuayfvektsstg.supabase.co',
+        pathname: '/storage/v1/object/public/course_images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'izefimwyuayfvektsstg.supabase.co',
+        pathname: '/storage/v1/object/public/settings_images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'izefimwyuayfvektsstg.supabase.co',
+        pathname: '/storage/v1/object/public/lesson_files/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'izefimwyuayfvektsstg.supabase.co',
+        pathname: '/storage/v1/object/public/announcement_attachments/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
