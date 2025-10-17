22aeaf216f6aab65ac7b9490f18666bb72f519ab
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-10-17 12:52:58 +0000

    los formularios no deben chocar con los laterales de la pantalla...

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 
 import * as React from "react"
@@ -41,6 +40,8 @@ const DialogContent = React.forwardRef<
       ref={ref}
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        // La corrección: se asegura de que en pantallas pequeñas el ancho sea del 95% y no del 100%
+        "w-[95vw] sm:w-full",
         className
       )}
       {...props}
