02512c8e332657c24a1448e25f8a228cf6bd5b78
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-10-21 19:44:22 +0000

    No me gusta que la linea este tan profundizada...

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -18,8 +18,8 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border/50",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        "shrink-0 bg-transparent", // Cambiado de bg-border a transparente
+        orientation === "horizontal" ? "h-auto w-full border-t" : "h-full w-auto border-l", // Usar bordes en lugar de fondo
         className
       )}
       {...props}
