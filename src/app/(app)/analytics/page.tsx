da40d8bc6b10895677f74ecf9bb82b52519f2971
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-10-29 19:28:06 +0000

    Quiero los numeros completos, despues del 15, sigue el 16, despúes del 1

--- src/app/(app)/analytics/page.tsx
+++ src/app/(app)/analytics/page.tsx
@@ -251,7 +251,7 @@ function AdminAnalyticsPage() {
                         <ResponsiveContainer>
                            <ComposedChart data={stats?.userRegistrationTrend || []} margin={{ top: 5, right: 5, left: 0, bottom: 5 }}>
                                 <CartesianGrid strokeDasharray="3 3" vertical={false} />
-                                <XAxis dataKey="date" tickLine={false} axisLine={true} tickMargin={10} tickFormatter={formatDateTick} interval={4} />
+                                <XAxis dataKey="date" tickLine={false} axisLine={true} tickMargin={10} tickFormatter={formatDateTick} interval={0} />
                                 <YAxis allowDecimals={false} tickLine={false} axisLine={true} tickMargin={10} width={30}/>
                                 <ChartTooltip cursor={{ fill: 'hsl(var(--muted))', radius: 4 }} content={<ChartTooltipContent indicator="dot" labelFormatter={formatDateTooltip} />} />
                                 <Legend />
