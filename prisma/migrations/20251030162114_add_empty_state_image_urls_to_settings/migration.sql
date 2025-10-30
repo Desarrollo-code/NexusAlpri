9fb348671fe73f1e09c4fcbc90317f1930196e15
vane24ofc <vaneaueisksow58@gmail.com>
2025-10-30 11:21:32 -0500

    .

---
+++ prisma/migrations/20251030162114_add_empty_state_image_urls_to_settings/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "platform_settings" ADD COLUMN     "emptyStateCertificatesUrl" TEXT,
+ADD COLUMN     "emptyStateCoursesUrl" TEXT,
+ADD COLUMN     "emptyStateFormsUrl" TEXT,
+ADD COLUMN     "emptyStateLeaderboardUrl" TEXT,
+ADD COLUMN     "emptyStateMotivationsUrl" TEXT,
+ADD COLUMN     "emptyStateMyCoursesUrl" TEXT,
+ADD COLUMN     "emptyStateMyNotesUrl" TEXT,
+ADD COLUMN     "emptyStateResourcesUrl" TEXT,
+ADD COLUMN     "emptyStateUsersUrl" TEXT;
