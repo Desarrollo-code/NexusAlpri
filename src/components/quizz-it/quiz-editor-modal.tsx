ff548bfe14e8911d1cf8e55ff0e11f18f3767e22
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-11-06 13:48:23 +0000

    Creo que el editor de quiz debería ser mucho más ancho para que tenmga u

--- src/components/quizz-it/quiz-editor-modal.tsx
+++ src/components/quizz-it/quiz-editor-modal.tsx
@@ -199,7 +199,7 @@ export function QuizEditorModal({ isOpen, onClose, quiz, onSave }: { isOpen: boo
     return (
       <>
         <Dialog open={isOpen} onOpenChange={onClose}>
-            <DialogContent className="max-w-4xl h-[90vh] flex flex-col p-0 gap-0 rounded-2xl">
+            <DialogContent className="max-w-5xl h-[90vh] flex flex-col p-0 gap-0 rounded-2xl">
                 <DialogHeader className="p-4 border-b flex-shrink-0">
                     <DialogTitle className="flex items-center gap-2"><Pencil className="h-5 w-5 text-primary"/>Editor de Quiz Interactivo</DialogTitle>
                 </DialogHeader>
