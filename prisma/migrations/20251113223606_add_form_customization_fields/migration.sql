d607200827e26aeb8906acfb96ca60beb4e1c296
vane24ofc <vaneaueisksow58@gmail.com>
2025-11-13 17:36:34 -0500

    .

---
+++ prisma/migrations/20251113223606_add_form_customization_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "forms" ADD COLUMN     "backgroundColor" TEXT,
+ADD COLUMN     "fontStyle" TEXT,
+ADD COLUMN     "headerImageUrl" TEXT,
+ADD COLUMN     "template" TEXT,
+ADD COLUMN     "themeColor" TEXT,
+ADD COLUMN     "timerStyle" TEXT;
