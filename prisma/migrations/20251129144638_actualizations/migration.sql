3fc8a10e06ac21fe34107d18611473270f56a10b
vane24ofc <vaneaueisksow58@gmail.com>
2025-11-29 09:46:50 -0500

    .

---
+++ prisma/migrations/20251129144638_actualizations/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "platform_settings" ADD COLUMN     "dashboardImageUrlAdmin" TEXT,
+ADD COLUMN     "dashboardImageUrlInstructor" TEXT,
+ADD COLUMN     "dashboardImageUrlStudent" TEXT,
+ADD COLUMN     "securityAuditImageUrl" TEXT,
+ADD COLUMN     "tourMascotUrl" TEXT;
