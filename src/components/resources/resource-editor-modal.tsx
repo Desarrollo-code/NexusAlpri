b18e243ec206fd6582fa175dd1b6c24cff095e80
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-12-04 21:26:24 +0000

    .

--- src/components/resources/resource-editor-modal.tsx
+++ src/components/resources/resource-editor-modal.tsx
@@ -334,7 +334,7 @@ export function ResourceEditorModal({ isOpen, onClose, resource, parentId, onSav
           </DialogDescription>
         </DialogHeader>
         
-        <ScrollArea className="flex-1 min-h-0 custom-scrollbar">
+        <ScrollArea className="flex-1">
           <form id="resource-form" onSubmit={handleSave} className="space-y-6 px-6 py-4">
             
             {/* Selector de Tipo de Recurso */}
