30f29c32ee4da210aa9125c6e3a39125e941137a
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-12-19 14:50:45 +0000

    Amplia un poco más los iconos

--- src/components/analytics/metric-card.tsx
+++ src/components/analytics/metric-card.tsx
@@ -40,7 +40,7 @@ export const MetricCard = ({
         >
              <div className="flex justify-between items-start text-primary-foreground/80">
                 <p className="text-sm font-semibold">{title}</p>
-                {Icon && <Icon className="h-6 w-6 md:h-8 md:w-8" />}
+                {Icon && <Icon className="h-6 w-6 md:h-10 md:w-10" />}
             </div>
             
             <div className="text-left">
