fd44954aad9274132b14c43df75bfb57c483cb73
Karol Nuñez <vaneaueisksow58@gmail.com>
2025-12-19 13:56:01 +0000

    Y ahora este para cursos publicados import * as React from "react"; cons

--- src/components/icons/icon-book-marked.tsx
+++ src/components/icons/icon-book-marked.tsx
@@ -3,56 +3,36 @@ import * as React from 'react';
 import { SVGProps } from 'react';
 
 export const IconBookMarked = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={48} height={48} viewBox="0 0 48 48" {...props}>
-    <path
-      d="M0 0 C3.96 0 7.92 0 12 0 C12 3.96 12 7.92 12 12 C8.04 12 4.08 12 0 12 C0 8.04 0 4.08 0 0 Z "
-      fill="#1665BF"
-      transform="translate(6,32)"
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <rect x={10} y={10} width={25} height={80} fill="#0078D4" />
+    <rect x={10} y={20} width={25} height={8} fill="#2B9EDF" />
+    <rect x={10} y={70} width={25} height={8} fill="#2B9EDF" />
+    <rect x={35} y={35} width={25} height={55} fill="#6A2BC3" />
+    <rect x={35} y={45} width={25} height={8} fill="#8E52E9" />
+    <rect x={60} y={10} width={25} height={80} fill="#A4262C" />
+    <rect x={60} y={20} width={25} height={8} fill="#D13438" />
+    <circle
+      cx={75}
+      cy={75}
+      r={22}
+      fill="#107C41"
+      stroke="black"
+      strokeWidth={0.5}
     />
     <path
-      d="M0 0 C3.96 0 7.92 0 12 0 C12 3.96 12 7.92 12 12 C8.04 12 4.08 12 0 12 C0 8.04 0 4.08 0 0 Z "
-      fill="#7FB547"
-      transform="translate(6,18)"
-    />
-    <path
-      d="M0 0 C3.96 0 7.92 0 12 0 C12 3.96 12 7.92 12 12 C8.04 12 4.08 12 0 12 C0 8.04 0 4.08 0 0 Z "
-      fill="#FEC312"
-      transform="translate(6,4)"
-    />
-    <path
-      d="M0 0 C6.6 0 13.2 0 20 0 C20 1.32 20 2.64 20 4 C13.4 4 6.8 4 0 4 C0 2.68 0 1.36 0 0 Z "
-      fill="#90CAF9"
-      transform="translate(22,36)"
-    />
-    <path
-      d="M0 0 C6.6 0 13.2 0 20 0 C20 1.32 20 2.64 20 4 C13.4 4 6.8 4 0 4 C0 2.68 0 1.36 0 0 Z "
-      fill="#90CAF9"
-      transform="translate(22,22)"
-    />
-    <path
-      d="M0 0 C6.6 0 13.2 0 20 0 C20 1.32 20 2.64 20 4 C13.4 4 6.8 4 0 4 C0 2.68 0 1.36 0 0 Z "
-      fill="#90CAF9"
-      transform="translate(22,8)"
-    />
-    <path
-      d="M0 0 C0 1.98 0 3.96 0 6 C-3.28696233 6.79953138 -4.71023192 7.09658936 -8 6 C-8 4.02 -8 2.04 -8 0 C-5.13822943 -1.43088528 -3.06624227 -0.59991697 0 0 Z "
-      fill="#EFF3DF"
-      transform="translate(16,21)"
-    />
-    <path
-      d="M0 0 C0.598125 0.226875 1.19625 0.45375 1.8125 0.6875 C2.5 2.5 2.5 2.5 2.8125 4.6875 C1.625 6.5 1.625 6.5 -0.1875 7.6875 C-2.375 7.375 -2.375 7.375 -4.1875 6.6875 C-4.875 4.875 -4.875 4.875 -5.1875 2.6875 C-3.58493056 0.24147296 -2.95482043 -0.4221172 0 0 Z "
-      fill="#F2CB4D"
-      transform="translate(13.1875,34.3125)"
-    />
-    <path
-      d="M0 0 C5.875 2.75 5.875 2.75 7 5 C3.7 5 0.4 5 -3 5 C-2.01 3.35 -1.02 1.7 0 0 Z "
-      fill="#E26C01"
-      transform="translate(10,9)"
-    />
-    <path
-      d="M0 0 C0.33 0 0.66 0 1 0 C1 1.98 1 3.96 1 6 C0.01 6.33 -0.98 6.66 -2 7 C-2 6.34 -2 5.68 -2 5 C-2.66 5 -3.32 5 -4 5 C-4 4.01 -4 3.02 -4 2 C-3.01 2 -2.02 2 -1 2 C-0.67 1.34 -0.34 0.68 0 0 Z "
-      fill="#E9E19B"
-      transform="translate(15,21)"
+      d="M65 75 L72 82 L85 68"
+      fill="none"
+      stroke="white"
+      strokeWidth={5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
+export default IconBookMarked;
